fc57c0507618d26c138b918d10f34d430f2b48c0
hyunahpark <phablue@gmail.com>
2014-06-23 10:25:36 -0500

    test sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,36 +4,64 @@ describe SessionsController do
   fixtures :users
 
   describe "POST 'create'" do
-    before(:each) do
-      @input_attributes = {
-        email: "john@example.com",
-        password: "password1",
-        password_confirmation: "password1" }
-    end
-
-    it "Redirect to order if successfully login" do
+    before(:each) {
       @user = users(:one)
-      post :create, user: @input_attributes
-      response.should redirect_to ('/')
+    }
+
+    context "If successfully login" do
+      it "user email equals session[:user_id]" do
+        @user.email.should_not eq session[:user_id]
+
+        post :create, email: @user.email, password: "password1"
+
+        @user.email.should eq session[:user_id]
+      end
+
+      it "Redirect to order" do
+        post :create, email: @user.email, password: "password1"
+
+        response.should redirect_to '/'
+      end
     end
 
-    it "Redirect to login_url if failed login" do
-      @user = users(:two)
-      post :create, user: @input_attributes
-      response.should redirect_to(login_url)
-    end
+    context "If failed login" do
+      it "session[:user_id] is nil" do
+        post :create, email: @user.email, password: "Failed"
 
-    it "Alert 'Invalid user/password combination' if failed login" do
-      @user = users(:two)
-      post :create, user: @input_attributes
-      flash[:alert].should eq "Invalid user/password combination"
+        session[:user_id].should be_nil
+      end
+
+      it "Redirect to login_url" do
+        post :create, email: @user.email, password: "Failed"
+
+        response.should redirect_to login_url
+      end
+
+      it "Alert 'Invalid user/password combination'" do
+        post :create, email: @user.email, password: "Failed"
+
+        flash[:alert].should eq "Invalid user/password combination"
+      end
     end
   end
 
   describe "DELETE 'destroy'" do
+    before(:each) {
+      post :create, email: users(:one).email, password: "password1"
+    }
+
+    it "session[:user_id] id nil" do
+      session[:user_id].should_not be_nil
+
+      delete :destroy
+
+      session[:user_id].should be_nil
+    end
+
     it "Redirect to user" do
       delete :destroy
-      response.should redirect_to ('/')
+
+      response.should redirect_to '/'
     end
   end
 end
