462b9dec55c27ee795b0283c0ec31b69a78eb081
hyunahpark <phablue@gmail.com>
2014-06-24 15:23:09 -0500

    user name db changed to firstname and last name

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,8 @@ ActiveRecord::Schema.define(version: 20140604193037) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.string   "department"
