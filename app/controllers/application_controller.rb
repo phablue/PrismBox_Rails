d8662571d6ea4259b4910019bfac2641499ba0ac
hyunahpark <phablue@gmail.com>
2014-06-25 10:34:57 -0500

    changed condition for accessing admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,20 @@ class ApplicationController < ActionController::Base
   before_action :authorize
   protect_from_forgery with: :exception
 
-  protected
+  private
+  def current_user
+    User.find_by_email(session[:user_id])
+  end
 
+  protected
   def authorize
-    unless User.find_by_email(session[:user_id])
+    unless current_user
       redirect_to login_url, notice: "Please log in"
     end
   end
 
   def admin_authorize
-    if User.find_by_email(session[:user_id]).admin
+    unless current_user && current_user.admin
       redirect_to '/', notice: "Sorry. This is a wrong access"
     end
   end
