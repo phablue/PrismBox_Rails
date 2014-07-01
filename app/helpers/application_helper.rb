aaeefae30517492f1793c515111201b5ef5637a4
hyunahpark <phablue@gmail.com>
2014-07-01 15:25:15 -0500

    link address changed depends on user character

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def current_user
     User.find_by_email(session[:user_id])
   end
+
+  def title_link
+    if current_user.nil?
+      return "/"
+    else
+      if current_user.admin
+        return admin_url
+      end
+      "/"
+    end
+  end
 end
