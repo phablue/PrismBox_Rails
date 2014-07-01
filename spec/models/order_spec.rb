4bffa6804932973d5f61df230eafbda356de3530
hyunahpark <phablue@gmail.com>
2014-07-01 15:56:49 -0500

    test order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Order do
-
+  it { should validate_presence_of :full_name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :laptop_serial_number }
 end
