3d153f53ec888a4555e0c613d31e8e182b16a745
hyunahpark <phablue@gmail.com>
2014-07-02 15:28:55 -0500

    duplication function'

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,8 +2,4 @@ module OrdersHelper
   def current_user_full_name
     "#{current_user.first_name.titlecase} #{current_user.last_name.titlecase}"
   end
-
-  def selected_laptop
-    session[:laptop_id]
-  end
 end
