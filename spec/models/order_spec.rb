bae108a62992b0b7f2440b2ffae5f91ec5860441
hyunahpark <phablue@gmail.com>
2014-08-09 19:52:43 -0500

    add belong_to test in order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,4 +4,6 @@ describe Order do
   it { should validate_presence_of :full_name }
   it { should validate_presence_of :email }
   it { should validate_presence_of :laptop_serial_number }
+  it { should belong_to(:user) }
+  it { should belong_to(:laptop) }
 end
