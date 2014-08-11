862249abe2af4ab1f61803f24fc4627d2be14750
hyunahpark <phablue@gmail.com>
2014-08-11 14:46:36 -0500

    apply new rsepc versioni syntax to catalog test

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -4,12 +4,12 @@ describe CatalogController do
   describe "GET 'index'" do
     it "returns http success" do
       get :index
-      response.should be_success
+      expect(response).to be_success
     end
 
     it "returns HTTP 200 status code" do
       get :index
-      response.status.should be 200
+      expect(response).to be_success
     end
   end
 end
